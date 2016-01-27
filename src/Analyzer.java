1a8bf6244264e163b9aba49d3282898cf1f0d294
Benom84 <menahem.ben@gmail.com>
2016-01-27 13:52:31 +0200

    Crawler should work
    Implemented Analyzer. Added SynchronizedSet.
    Threads update active count on crawler.
    Need to add report at the end of the crawler

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -1,13 +1,58 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Analyzer implements Runnable {
+	
+	private Crawler parentCrawler;
+	
 	public Analyzer(Crawler crawler) {
+		
+		parentCrawler = crawler;
 		
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		String currentHtmlToParse;
+		while (true) {
+			
+			try {
+				currentHtmlToParse = parentCrawler.nextHtmlToAnalyze();
+				parentCrawler.AdjustWorkingThreadCount(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				continue;
+			}
+			
+			Set<String> allLinksInHtml = getLinkesFromHtml(currentHtmlToParse);
+			for (String currentLink : allLinksInHtml) {
+				try {
+					parentCrawler.addUrlToDownload(currentLink);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			parentCrawler.AdjustWorkingThreadCount(-1);
+			
+		}
 		
 	}
+	
+	public Set<String> getLinkesFromHtml(String HTMLPage){
+
+        Pattern linkPattern = Pattern.compile("href=\\'.*?(http:\\/\\/)*(.*?)\\'",  Pattern.CASE_INSENSITIVE|Pattern.DOTALL);
+        Matcher pageMatcher = linkPattern.matcher(HTMLPage);
+        Set<String> links = new HashSet<>();
+        while(pageMatcher.find()){
+            links.add(pageMatcher.group(2));
+            System.out.println(pageMatcher.group(2));
+        }
+
+        return links;
+    }
 
 }
